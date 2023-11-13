740421a19082ca4bc98732fee2d724011497c5a9
Javier <fjgallardo00@gmail.com>
2023-11-13 23:26:06 +0100

    Feat functional tfg

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ FROM ubuntu:latest
 
 RUN apt-get update && apt-get install -y apt-utils sudo
 RUN sudo apt update
-RUN sudo apt-get install -y mininet iproute2 wget iputils-ping make traceroute iperf3 git pip systemctl
+RUN sudo apt-get install -y mininet iproute2 wget iputils-ping make traceroute iperf3 git pip systemctl curl
+RUN sudo apt-get install -y tcpdump
 
 WORKDIR /home/app/mininet-app/
--- app/mininet-app/mininet-network.py
+++ app/mininet-app/mininet-network.py
@@ -6,12 +6,16 @@ from mininet.cli import CLI
 
 class NetworkTopo(Topo):
     def build(self, **_opts):
+        # self.doble_anillo()
+        # self.estrella_extendida()
+        self.personalizada()
 
-        sn1 = self.addHost('sn1', ip='10.1.0.1/24')
-        sn2 = self.addHost('sn2', ip='10.1.0.2/24')
-        sn3 = self.addHost('sn3', ip='10.1.0.3/24')
-
-        dn1 = self.addHost('dn1', ip='10.1.0.4/24')
+    def personalizada(self):
+        h1 = self.addHost('h1', ip='10.1.0.1/24')
+        h2 = self.addHost('h2', ip='10.1.0.2/24')
+        h3 = self.addHost('h3', ip='10.1.0.3/24')
+        h4 = self.addHost('h4', ip='10.1.0.4/24')
+        h5 = self.addHost('h5', ip='10.1.0.5/24')
 
         s1 = self.addSwitch('s1')
         s2 = self.addSwitch('s2')
@@ -21,25 +25,84 @@ class NetworkTopo(Topo):
         s6 = self.addSwitch('s6')
         s7 = self.addSwitch('s7')
 
-        self.addLink(sn1, s1)
-        self.addLink(sn2, s1)
-        self.addLink(sn3, s1)
+        self.addLink(h1, s1)
+        self.addLink(h2, s1)
+        self.addLink(h3, s1)
+        self.addLink(h4, s2)
+        self.addLink(h5, s7)
 
-        self.addLink(dn1, s2)
-        #
-        # # Path 0
+        # Path 0
         self.addLink(s1, s2)
-        #
-        # # Path 1
+
+        # Path 1
         self.addLink(s1, s3)
         self.addLink(s3, s4)
         self.addLink(s4, s2)
-        #
-        # # Path 2
+
+        # Path 2
         self.addLink(s1, s5)
         self.addLink(s5, s6)
         self.addLink(s6, s7)
         self.addLink(s7, s2)
+
+    def doble_anillo(self):
+
+        h1 = self.addHost('h1', ip='10.1.0.1/24')
+        h2 = self.addHost('h2', ip='10.1.0.2/24')
+        h3 = self.addHost('h3', ip='10.1.0.3/24')
+        h4 = self.addHost('h4', ip='10.1.0.4/24')
+
+        s1 = self.addSwitch('s1')
+        s2 = self.addSwitch('s2')
+        s3 = self.addSwitch('s3')
+        s4 = self.addSwitch('s4')
+
+        # Anillo 1
+        self.addLink(h1, s1)
+        self.addLink(h2, s1)
+        self.addLink(s1, s2)
+        self.addLink(s1, s3)
+        self.addLink(s2, s3)
+
+        # Anillo 2
+        self.addLink(h4, s4)
+        self.addLink(h3, s4)
+        self.addLink(s2, s4)
+        self.addLink(s3, s4)
+
+    def estrella_extendida(self):
+        h1 = self.addHost('h1', ip='10.1.0.1/24')
+        h2 = self.addHost('h2', ip='10.1.0.2/24')
+        h3 = self.addHost('h3', ip='10.1.0.3/24')
+        h4 = self.addHost('h4', ip='10.1.0.4/24')
+        h5 = self.addHost('h5', ip='10.1.0.5/24')
+        h6 = self.addHost('h6', ip='10.1.0.6/24')
+        h7 = self.addHost('h7', ip='10.1.0.7/24')
+
+        s1 = self.addSwitch('s1')
+        s2 = self.addSwitch('s2')
+        s3 = self.addSwitch('s3')
+        s4 = self.addSwitch('s4')
+        s5 = self.addSwitch('s5')
+
+        # Punta 1
+        self.addLink(s2, h1)
+        self.addLink(s2, h2)
+        self.addLink(s2, s1)
+
+        # Punta 2
+        self.addLink(s3, h3)
+        self.addLink(s3, h4)
+        self.addLink(s3, s1)
+
+        # Punta 3
+        self.addLink(s4, h5)
+        self.addLink(s4, h6)
+        self.addLink(s4, s1)
+
+        # Punta 4
+        self.addLink(s5, h7)
+        self.addLink(s5, s1)
 
 
 def installFlows(net):
